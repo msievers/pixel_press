7d2382bde6c5b84345be1a2a0bfdd9412bb4b476
Michael Sievers <msievers@users.noreply.github.com>
2017-02-23 11:18:31 +0100

    WIP

--- lib/pixel_press/base.rb
+++ lib/pixel_press/base.rb
@@ -1,17 +1,18 @@
 module PixelPress
   class Base < ActionController::Base
     def self.template(name, options = {}, &block)
+      # define an instance method for this template
       define_method name do |*args|
         instance_exec(*args, &block)
-        filename = instance_eval(&options[:filename]) if options[:filename].respond_to?(:call)
         html = render_to_string "#{controller_path}/#{__method__}.pdf", formats: [:html]
         pdf = WeasyPrint.new(html).to_pdf
         stream = PixelPress::StreamablePDF.new(pdf)
-        stream.original_filename = filename || "document.pdf"
+        stream.original_filename = "document.pdf"
         stream.content_type = "application/pdf"
         stream
       end
 
+      # make it also accessible as a class method
       define_singleton_method name do |*args|
         new.send(name, *args)
       end
